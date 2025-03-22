34ae21b350997800ace0f55a3e97c671aa22f8e2
kaimon0 <luandasarah1031@gmail.com>
2025-03-22 23:57:19 +0100

    feet(resquests): [ADD] class Requests http

---
+++ ghibli_server/src/Http/Requests.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Src\Http;
+
+
+class Requests
+{
+    private static  $instance = null;
+
+    private function __construct(
+        private array $server,
+        private array $get,
+        private array $post,
+        private array $files,
+        private array $cookies,
+        private array $env
+    ) {}
+
+    public static function create(): static
+    {
+        if (static::$instance === null) {
+            static::$instance = new static(
+                $_SERVER,
+                $_GET,
+                $_POST,
+                $_FILES,
+                $_COOKIE,
+                $_ENV
+            );
+        }
+
+        return static::$instance;
+    }
+}
